535ff796fd9e711e34240f3ea5039db223f7fea3
Ivan <ab09@inbox.ru>
2022-12-05 00:20:46 +0500

    Added method of creating new sorted massive

--- ControlTask/Program.cs
+++ ControlTask/Program.cs
@@ -22,6 +22,27 @@
                 return Mass;
             }
 
+            
+            // Создаем новый массив из элементов, удовлетворяющих условию задачи
+
+            string[] GetNewMass(string[] array)
+            {
+                string[] Mass = Array.Empty<string>();
+                int j = 0;
+                for (int i = 0; i < array.Length; i++)
+                {
+
+                    if (array[i].Length < 4)
+                    {
+                        Array.Resize(ref Mass, Mass.Length + 1);
+                        Mass[j] = array[i];
+                        j++;
+                    }
+
+                }
+                return Mass;
+            }
+
 
 
 
