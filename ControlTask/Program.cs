c0f7f9300bbe469ffc071b29207f97560f05d34c
Ivan <ab09@inbox.ru>
2022-12-05 00:20:00 +0500

    Added method of creating string massive

---
+++ ControlTask/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Name
+{
+    class Program
+    {
+        static void Main()
+        {
+             Console.Clear();
+
+            // Создаем массив строк
+
+            string[] OriginalMass(int length)
+            {
+                string[] Mass = new string[length];
+                int i = 0;
+                while (i < length)
+                {
+                    Console.WriteLine("Введите строку массива ");
+                    Mass[i] = Console.ReadLine();
+                    i++;
+                }
+                Console.WriteLine();
+                return Mass;
+            }
+
+
+
+
+        }
+    }
+}
